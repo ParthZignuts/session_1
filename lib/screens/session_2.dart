1ed484d1b4f6e46d6b31f6b30e13990b928e9821
ParthAkbari <partha@zignuts.com>
2023-02-03 18:33:15 +0530

    Adding Some Features NavigationRail and Maping the List element which is Favorite

--- lib/screens/session_2.dart
+++ lib/screens/session_2.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:like_button/like_button.dart';
-import 'package:session_1/main.dart';
+import 'dart:collection';
 
 class Session2 extends StatefulWidget {
   @override
@@ -9,70 +8,163 @@ class Session2 extends StatefulWidget {
 
 class _Session2State extends State<Session2> {
   bool likeState = false;
+  var selectedIndex = 0;
+  var names = ['Java', 'Flutter', 'Dart', 'c', 'react', 'js', 'node'];
+  var favoriteItems = [];
+
+  String txtNames = 'c++';
+  int counter = 0;
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-        // primarySwatch: Colors.lime,
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.lime),
+    //List Containing Widgets To show on Cliking of Navigation Rail
+    List<Widget> likePage = [
+      //This is for Home Page
+      Container(
+        alignment: Alignment.center,
+        color: Colors.orangeAccent,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.all(Radius.circular(20)),
+              ),
+              height: 80,
+              width: 200,
+              child: Card(
+                child: Center(
+                  child: Text(
+                    "Light Stream",
+                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.w900),
+                  ),
+                ),
+                color: Colors.lime,
+                shadowColor: Colors.black,
+                elevation: 30,
+              ),
+            ),
+            Flexible(
+              child: Text(
+                txtNames,
+                style: TextStyle(fontWeight: FontWeight.w900, fontSize: 25),
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton.icon(
+                  icon: Icon(
+                    Icons.favorite,
+                    color: likeState ? Colors.white : Colors.red,
+                  ),
+                  onPressed: () => {
+                    setState(() {
+                      likeState = !likeState;
+                      favoriteItems.add(txtNames);
+                    }),
+                    print("Button Pressed"),
+                  },
+                  label: Text(
+                    "Like",
+                    style: TextStyle(
+                        fontWeight: FontWeight.w900,
+                        fontSize: 20,
+                        color: Colors.black),
+                  ),
+                ),
+                SizedBox(
+                  width: 10,
+                ),
+                ElevatedButton(
+                  onPressed: () => {
+                    setState(() {
+                      if (counter <= names.length) {
+                        txtNames = names[counter];
+                      }
+                      counter++;
+                      print(txtNames);
+                      print(names);
+                    }),
+                  },
+                  child: Text(
+                    "Next",
+                    style: TextStyle(
+                        fontWeight: FontWeight.w900,
+                        fontSize: 20,
+                        color: Colors.black),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
 
-      debugShowCheckedModeBanner: false,
+
+
+      //this is for Liked Page
+      Container(
+        alignment: Alignment.center,
+        decoration: BoxDecoration(color: Colors.orangeAccent),
+        child: favoriteItems.isEmpty
+            ? Center(
+                child: Text("No Any Favorite Item Yet"),
+              )
+            : ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: 1,
+                itemBuilder: (BuildContext context, int index) {
+                  return Container(
+                    child: Text(
+                      favoriteItems.toString(),
+                    ),
+                  );
+                }),
+
+      ),
+    ];
+
+
+
+
+    return MaterialApp(
       // This line is used for desable the debug banner
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.lime,
+      ),
       title: "Session 2",
       home: Scaffold(
         appBar: AppBar(
           centerTitle: true,
           title: Text("Session 2"),
         ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Container(
-                decoration: BoxDecoration(
-                    borderRadius: BorderRadius.all(Radius.circular(20))),
-                height: 80,
-                width: 200,
-                child: Card(
-                  child: Center(
-                    child: Text(
-                      "Light Stream",
-                      style:
-                          TextStyle(fontSize: 20, fontWeight: FontWeight.w900),
-                    ),
-                  ),
-                  color: Colors.green,
-                  shadowColor: Colors.black,
-                  elevation: 30,
+        body: Row(
+          children: [
+            NavigationRail(
+              labelType: NavigationRailLabelType.selected,
+              destinations: [
+                NavigationRailDestination(
+                  icon: Icon(Icons.home),
+                  label: Text("Home"),
                 ),
-              ),
-              Text(
-                "A Random Awesome Idea:",
-                style: TextStyle(fontWeight: FontWeight.w900, fontSize: 35),
-              ),
-              ElevatedButton.icon(
-                icon: Icon(
-                  Icons.favorite,
-                  color: likeState ? Colors.white : Colors.red,
+                NavigationRailDestination(
+                  icon: Icon(Icons.favorite),
+                  label: Text("Favorite"),
                 ),
-                onPressed: () => {
-                  setState(() {
-                    likeState = !likeState;
-                  }),
-                  print("Button Pressed"),
-                },
-                label: Text(
-                  "Next",
-                  style: TextStyle(
-                      fontWeight: FontWeight.w900,
-                      fontSize: 20,
-                      color: Colors.black),
-                ),
-              ),
-            ],
-          ),
+              ],
+              selectedIndex: selectedIndex,
+              onDestinationSelected: (index) {
+                setState(() {
+                  selectedIndex = index;
+                });
+              },
+            ),
+            Expanded(
+              child: likePage[selectedIndex],
+            ),
+          ],
         ),
       ),
     );
